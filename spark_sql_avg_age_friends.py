041dcaaa19062eba5a183e17847f9192daec95e1
Scott Chen <yanbin.chen.cn@gmail.com>
2024-04-01 21:56:32 +0800

    add master("local[*]")

--- spark_sql_avg_age_friends.py
+++ spark_sql_avg_age_friends.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession
 from dataset.utils import find_absolute_path
 
+# "local[4]" 中的 4 个线程是由 executor 使用的，用于并行执行任务。driver 程序通常只使用一个线程来控制应用程序的流程。
 spark = SparkSession.Builder().appName(
-    "Use DataFrame to aggregate").getOrCreate()
+    "Use DataFrame to aggregate").master("local[*]").getOrCreate()
 
 fakefriends = spark.read.option("header", "true").option(
     "inferSchema", "true").csv(find_absolute_path("fakefriends-header.csv"))
